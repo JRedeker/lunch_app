fb030974026a0b6324a1593dd39862cc187ec2f8
Jon Redeker <jon.redeker@gmail.com>
2015-11-27 17:38:51 -0500

    change!

--- static/js/location.js
+++ static/js/location.js
@@ -11,9 +11,11 @@ function showPosition(position) {
     lat = position.coords.latitude;
     lon = position.coords.longitude;
     latlon = new google.maps.LatLng(lat, lon)
+    var width = window.screen.availWidth * (.7);
+    var height = width * .5;
     mapholder = document.getElementById('mapholder')
-    mapholder.style.height = '250px';
-    mapholder.style.width = '500px';
+    mapholder.style.height = height+'px';
+    mapholder.style.width = width;
 
     var myOptions = {
     center:latlon,zoom:14,
@@ -22,8 +24,16 @@ function showPosition(position) {
     navigationControlOptions:{style:google.maps.NavigationControlStyle.SMALL}
     }
     
+    updateAddress(latlon);
     var map = new google.maps.Map(document.getElementById("mapholder"), myOptions);
     var marker = new google.maps.Marker({position:latlon,map:map,title:"You are here!"});
+}
+
+function updateAddress(latlon) {
+    var address = $.ajax("http://maps.googleapis.com/maps/api/geocode/json?latlng=44.4647452,7.3553838&sensor=true)");
+    console.log(address);
+
+    
 }
 
 function showError(error) {
