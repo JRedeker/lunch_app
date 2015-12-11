cc14ef3bf626a99c8352f2be3eaa4202ab77c621
Jon Redeker <jon.redeker@gmail.com>
2015-12-11 18:28:25 -0500

    added gulp concat

--- lunch_app/gulpfile.js
+++ lunch_app/gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp');
 var bower = require('gulp-bower');
-
+// Include plugins
+var concat = require('gulp-concat');
 gulp.task('default', function() {
     // place code for your default task here
 });
-
+// Concatenate JS Files
+gulp.task('scripts', function() {
+    return gulp.src('src/js/*.js')
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('build/js'));
+});
 gulp.task('bower', function() {
     return bower('./static/js/bower_components')
         .pipe(gulp.dest('lib/'))
