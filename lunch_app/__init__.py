61ebe4be0dbf788773734e18de53050a50e90eed
Jon Redeker <jon.redeker@gmail.com>
2015-12-22 16:59:47 -0500

    new index page

--- lunch_app/__init__.py
+++ lunch_app/__init__.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index-new.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -14,13 +14,9 @@ def page_not_found(e):
 def page_not_found(e):
     return render_template('500.html')
 
-@app.route("/test")
+@app.route("/old")
 def test():
-    return render_template('index-test.html')
-
-@app.route("/card")
-def card():
-    return render_template('card.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8080)
