f9e579b5628a333a5b9d989bcd67fe0eb5a8c11d
Ben Marti <kareas1115@gmail.com>
2015-12-26 23:36:47 -0500

    Exclude food types dropdown

--- lunch_app/static/js/request.js
+++ lunch_app/static/js/request.js
@@ -9,3 +9,9 @@ function sendRequest(n) {
 	console.log(lat + " " + lon + " " + numberOfPlaces + " " + " " + radius)
 
 }
+
+// Allow Bootstrap dropdown menus to have forms/checkboxes inside, 
+// and when clicking on a dropdown item, the menu doesn't disappear.
+$(document).on('click', '.dropdown-menu.dropdown-menu-form', function(e) {
+  e.stopPropagation();
+});
